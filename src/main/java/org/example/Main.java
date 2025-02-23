41cdd0dc63ee0784c0708b56870ef137e719c353
blin_ <peresvetzhigulin@gmail.com>
2025-02-24 02:40:44 +0300

    дома

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,55 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> question = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
+        ArrayList<String> trueAnswer = new ArrayList<>();
+        System.out.println("Добро пожаловать в прорграмму по созданию и проигравынию викторин,вводите вопросы которые хотетите задать участникам,когда вопросы закончатся напишите exit");
+        while (true) {
+            System.out.println("Введите вопрос:");
+            String rawQuestion = scanner.nextLine();
+            if (rawQuestion.equalsIgnoreCase("exit")) {
+                break;
+            }
+            System.out.println("Введите 4 варианта ответа,последним ОБЯЗАТЕЛЬНО вариант который будет являтся ответом: ");
+            String rawAnswer = scanner.nextLine();
+            String[] answers = rawAnswer.split(", ");
+            trueAnswer.add(answers[answers.length - 1]);
+            answer.add(rawAnswer);
+            question.add(rawQuestion);
         }
+        System.out.println("Внимание вопрос");
+        while (true) {
+            for (int i = 0; i < question.size(); i++) {
+                System.out.println(question.get(i));
+                if (i > 0) {
+                    System.out.println(answer.get(i * 4));
+                    System.out.println(answer.get(i * 4 + 1));
+                    System.out.println(answer.get(i * 4 + 2));
+                    System.out.println(answer.get(i * 4 + 3));
+                }
+                else
+                    System.out.println(answer.get(i));
+                System.out.println(answer.get(i + 1));
+                System.out.println(answer.get(i + 2));
+                System.out.println(answer.get(i + 3));
+                System.out.println("Выберите вариант ответа: ");
+                String answerFromUser = scanner.nextLine();
+                if (answerFromUser.equalsIgnoreCase(trueAnswer.get(i))) {
+                    System.out.println("Правильно!");
+                } else {
+                    System.out.println("Неправильно :(");
+                }
+
+            }
+            break;
+        }
+        }
+
     }
-}
